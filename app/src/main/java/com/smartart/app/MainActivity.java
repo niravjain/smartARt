b2a96386aeed3cf1edeeb13f463527ba75d5ab66
Chirag Jain <csjain@ncsu.edu>
2018-04-22 02:49:08 -0400

    Home screen done

--- app/src/main/java/com/smartart/app/MainActivity.java
+++ app/src/main/java/com/smartart/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.smartart.app;
 
 import android.Manifest;
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
@@ -113,14 +115,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
         mainContext = this;
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.drawButton).setOnClickListener(drawOnCanvasListener);
-        findViewById(R.id.photoButton).setOnClickListener(photoOnCanvasListener);
-        findViewById(R.id.button3).setOnClickListener(min3dOnCanvasListener);
+        Button drawButton = findViewById(R.id.drawButton);
+        drawButton.setOnClickListener(drawOnCanvasListener);
+
+        drawButton.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
